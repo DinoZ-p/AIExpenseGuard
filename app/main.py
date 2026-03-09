c8d631964d74f122c7d7c13b0c73e21f09582c59
DinoZ-p <tomzhaozhenghang@gmail.com>
2026-03-09 12:04:15 -0400

    added password changing

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import auth, categories, transactions, goals, budgets, analytics, advisor
+from app.routers import auth, categories, transactions, goals, budgets, analytics, rules, advisor
 
 app = FastAPI(title="SpendingTrack", version="0.1.0")
 
@@ -9,4 +9,5 @@ app.include_router(transactions.router)
 app.include_router(goals.router)
 app.include_router(budgets.router)
 app.include_router(analytics.router)
+app.include_router(rules.router)
 app.include_router(advisor.router)
